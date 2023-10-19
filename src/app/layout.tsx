beab4be83a554c22c0a764aec393eb4ff3134280
nondefault <spam@nondefault.net>
2023-10-19 02:25:20 -0700

    new font stack: work sans

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,14 +2,24 @@ import 'highlight.js/styles/base16/material-palenight.css';
 import '@/app/globals.css';
 
 import type {Metadata} from 'next';
-import {DM_Mono, DM_Serif_Display, DM_Serif_Text, Sora} from 'next/font/google';
+import {
+  DM_Mono,
+  DM_Serif_Display,
+  DM_Serif_Text,
+  Work_Sans,
+} from 'next/font/google';
 
 const display = DM_Serif_Display({
   subsets: ['latin'],
   weight: '400',
   variable: '--font-serif',
 });
-const sans = Sora({subsets: ['latin'], variable: '--font-sans'});
+const sans = Work_Sans({
+  subsets: ['latin'],
+  variable: '--font-sans',
+  style: ['normal', 'italic'],
+  weight: 'variable',
+});
 const serif = DM_Serif_Text({
   subsets: ['latin'],
   weight: '400',
@@ -25,6 +35,7 @@ export const metadata: Metadata = {
   title: 'loganz',
   description: "Logan's homepage",
   authors: [{name: 'Logan Zartman'}],
+  viewport: 'width=device-width, initial-scale=1',
 };
 
 export default function RootLayout({children}: {children: React.ReactNode}) {

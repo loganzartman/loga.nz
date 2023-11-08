87577892d6122e3e656820b43717f77249c5ff9a
Logan Zartman <logan.zartman@utexas.edu>
2023-11-08 18:43:00 -0500

    update diy suspense, add admonitions

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,7 +1,10 @@
 import type {MDXComponents} from 'mdx/types';
 
+import Admonition from '@/lib/components/Admonition';
+
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
     ...components,
+    Admonition,
   };
 }
---
+++ src/lib/components/Admonition.tsx
@@ -0,0 +1,36 @@
+export type Type = 'note' | 'warning';
+
+const icons: Record<Type, React.ReactNode> = {
+  note: <div>🛈</div>,
+  warning: <div>⚠</div>,
+};
+
+const messages: Record<Type, React.ReactNode> = {
+  note: <div>Note</div>,
+  warning: <div>Warning!</div>,
+};
+
+const classNames: Record<Type, string> = {
+  note: 'text-indigo-100 bg-indigo-500/10 ring-indigo-500',
+  warning: 'text-amber-100 bg-amber-500/10 ring-amber-500',
+};
+
+export default function Admonition({
+  children,
+  type,
+}: {
+  children: React.ReactNode;
+  type: Type;
+}) {
+  return (
+    <div
+      className={`flex flex-col prose-p:m-0 p-4 rounded-lg ring-1 ${classNames[type]}`}
+    >
+      <div className="flex flex-row items-start gap-2 mb-2 font-bold lowercase">
+        {icons[type]}
+        {messages[type]}
+      </div>
+      <div>{children}</div>
+    </div>
+  );
+}

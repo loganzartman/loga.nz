ecbe919b2411dcf5983cb23d6c270d592158ba38
Logan <loganzartman@live.com>
2023-11-15 16:00:24 -0800

    set metadataBase (#26)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -34,6 +34,11 @@ const mono = DM_Mono({
 });
 
 export const metadata: Metadata = {
+  metadataBase: new URL(
+    process.env.NODE_ENV === 'production'
+      ? 'https://loga.nz'
+      : `http://localhost:${process.env.PORT || 3000}`,
+  ),
   title: 'loganz',
   description: "Logan's homepage",
   authors: [{name: 'Logan Zartman'}],

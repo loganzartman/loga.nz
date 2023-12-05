e2fdb725558b8d6e3012acd81bfee363c33adf6b
nondefault <spam@nondefault.net>
2023-12-05 14:51:47 -0800

    rework animation

--- src/app/blog/posts/consistent-hashing/Ring.tsx
+++ src/app/blog/posts/consistent-hashing/Ring.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import {AnimatePresence, motion} from 'framer-motion';
+import {useMemo} from 'react';
 
 import {hash} from '@/app/blog/posts/consistent-hashing/hash-string';
 
@@ -12,7 +13,6 @@ type Props = {
 };
 
 function pointsToD(points: number[][]) {
-  console.log(points);
   return points
     .map(
       (p, i) => `${i === 0 ? 'M' : 'L'}${p[0].toFixed(3)},${p[1].toFixed(3)}`,
@@ -24,46 +24,76 @@ export default function Ring({servers, keys, circle, seed}: Props) {
   const n = 64;
   const mod = 2 ** 32 - 1;
 
-  const variants = {
-    line: {
-      d: pointsToD(Array.from({length: n}).map((_, i) => [(i / n) * 2 - 1, 0])),
-    },
-    circle: {
-      d: pointsToD(
-        Array.from({length: n}).map((_, i) => {
-          const f = (i / (n - 1) + 0.25) * 2 * Math.PI;
-          return [Math.cos(f), Math.sin(f)];
-        }),
-      ),
-    },
-  };
+  const transition = useMemo(
+    () => ({type: 'spring', mass: 1, stiffness: 80, damping: 20}),
+    [],
+  );
+
+  const variants = useMemo(
+    () => ({
+      line: {
+        d: pointsToD(
+          Array.from({length: n}).map((_, i) => [(i / n) * 2 - 1, 0]),
+        ),
+      },
+      circle: {
+        d: pointsToD(
+          Array.from({length: n}).map((_, i) => {
+            const f = (i / (n - 1) + 0.25) * 2 * Math.PI;
+            return [Math.cos(f), Math.sin(f)];
+          }),
+        ),
+      },
+    }),
+    [],
+  );
+
+  const rotateVariants = useMemo(
+    () => ({
+      line: {rotate: '0'},
+      circle: {rotate: '180deg'},
+    }),
+    [],
+  );
+
+  const invRotateVariants = useMemo(
+    () => ({
+      line: {rotate: '0'},
+      circle: {rotate: '-180deg'},
+    }),
+    [],
+  );
 
   const serverIcons = servers.map((server, i) => {
     const h = hash(server, seed);
     const pos = h / mod;
-    const circleProps = {
-      translateX: Math.cos((pos + 0.25) * Math.PI * 2),
-      translateY: Math.sin((pos + 0.25) * Math.PI * 2),
-      scale: 1,
-      opacity: 1,
-      fill: `hsl(${(i / servers.length) * 0.9 * 360}, 80%, 80%)`,
-    };
-    const lineProps = {
-      translateX: pos * 2 - 1,
-      translateY: 0,
-      scale: 1,
-      opacity: 1,
-      fill: `hsl(${(i / servers.length) * 0.9 * 360}, 80%, 80%)`,
+
+    const variants = {
+      line: {
+        ...invRotateVariants.line,
+        translateX: pos * 2 - 1,
+        translateY: 0,
+        scale: 1,
+        opacity: 1,
+        fill: `hsl(${(i / servers.length) * 0.9 * 360}, 80%, 80%)`,
+      },
+      circle: {
+        ...invRotateVariants.circle,
+        translateX: Math.cos((pos + 0.25) * Math.PI * 2),
+        translateY: Math.sin((pos + 0.25) * Math.PI * 2),
+        scale: 1,
+        opacity: 1,
+        fill: `hsl(${(i / servers.length) * 0.9 * 360}, 80%, 80%)`,
+      },
     };
 
     return (
       <motion.g
         key={server}
-        custom={i}
         initial={{opacity: 0, scale: 0, translateX: 0, translateY: 0}}
         exit={{opacity: 0, scale: 0, translateX: 0, translateY: 0}}
-        animate={circle ? circleProps : lineProps}
-        transition={{type: 'spring', stiffness: 200, damping: 40}}
+        animate={circle ? variants.circle : variants.line}
+        transition={transition}
       >
         <circle cx={0} cy={0} r={0.05} />
         <text
@@ -87,14 +117,16 @@ export default function Ring({servers, keys, circle, seed}: Props) {
       viewBox="-1.2 -1.2 2.4 2.4"
       animate={circle ? 'circle' : 'line'}
     >
-      <motion.path
-        variants={variants}
-        className="stroke-brand-200"
-        strokeWidth="2"
-        fill="none"
-        transition={{type: 'spring', stiffness: 200, damping: 40}}
-      />
-      <AnimatePresence>{serverIcons}</AnimatePresence>
+      <motion.g variants={rotateVariants} transition={transition}>
+        <motion.path
+          variants={variants}
+          className="stroke-brand-200"
+          strokeWidth="2"
+          fill="none"
+          transition={transition}
+        />
+        <AnimatePresence>{serverIcons}</AnimatePresence>
+      </motion.g>
     </motion.svg>
   );
 }

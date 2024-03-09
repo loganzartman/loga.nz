92b559d5194d2ae1cf36ea8692fada0182ece35a
Logan <logan.zartman@utexas.edu>
2024-03-09 15:46:53 -0800

    sketch out layer functionality

--- src/app/reacjin/ReacjinEditor.tsx
+++ src/app/reacjin/ReacjinEditor.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import {Nunito, Overpass, Work_Sans} from 'next/font/google';
-import {useCallback, useRef, useState} from 'react';
+import {useCallback, useEffect, useRef, useState} from 'react';
 
 import {Button} from '@/app/reacjin/Button';
 import styles from '@/app/reacjin/styles.module.css';
@@ -12,16 +12,64 @@ const overpass = Overpass({weight: 'variable', preload: false});
 
 const fonts = [nunito, overpass, workSans];
 
+function uuid() {
+  return window.crypto.randomUUID();
+}
+
+type RenderFunction<O> = (ctx: CanvasRenderingContext2D, options: O) => void;
+
+type LayerPlugin<N, O> = {name: N; render: RenderFunction<O>};
+
+const image = new Image();
+image.src =
+  'https://i.natgeofe.com/n/4cebbf38-5df4-4ed0-864a-4ebeb64d33a4/NationalGeographic_1468962_4x3.jpg?w=256&h=256';
+
+const imageLayerPlugin: LayerPlugin<'image', {}> = {
+  name: 'image',
+  render: (ctx, options) => {
+    ctx.drawImage(image, 0, 0);
+  },
+};
+
+const textLayerPlugin: LayerPlugin<'text', {}> = {
+  name: 'text',
+  render: (ctx, options) => {
+    ctx.fillStyle = 'black';
+    ctx.textAlign = 'center';
+    ctx.textBaseline = 'middle';
+    ctx.fillText('Test', ctx.canvas.width / 2, ctx.canvas.height / 2);
+  },
+};
+
+const layerPlugins = {
+  image: imageLayerPlugin,
+  text: textLayerPlugin,
+} as const;
+
+type Layer = {id: string; plugin: keyof typeof layerPlugins; options: unknown};
+
 function ImageCanvas({
   width,
   height,
   zoom,
+  layers,
 }: {
   width: number;
   height: number;
   zoom: number;
+  layers: Layer[];
 }) {
   const canvasRef = useRef<HTMLCanvasElement>(null);
+
+  useEffect(() => {
+    if (!canvasRef.current) return;
+    const ctx = canvasRef.current.getContext('2d')!;
+
+    for (const layer of layers) {
+      layerPlugins[layer.plugin].render(ctx, {});
+    }
+  }, [layers]);
+
   return (
     <div className="p-1 shadow-lg rounded-md ring-1 ring-[rgba(255,255,255,0.5)]">
       <canvas
@@ -38,9 +86,28 @@ function ImageCanvas({
   );
 }
 
+function LayerPane({layers}: {layers: Layer[]}) {
+  const layersDisplay = layers.map((layer) => (
+    <div key={layer.id} className="p-2 transition-colors hover:bg-brand-400/20">
+      {layer.plugin}
+    </div>
+  ));
+
+  return (
+    <div className="w-[20ch] bg-background ring-1 ring-brand-200 rounded-lg m-2 flex flex-col">
+      <div className="p-2 bg-brand-100/10">Layers</div>
+      {layersDisplay}
+    </div>
+  );
+}
+
 export function ReacjinEditor() {
   const [imageSize, setImageSize] = useState([256, 256]);
   const [zoom, setZoom] = useState(1);
+  const [layers, setLayers] = useState<Layer[]>([
+    {id: '0', plugin: 'image', options: {}},
+    {id: '1', plugin: 'text', options: {}},
+  ]);
 
   const setZoomToSize = useCallback(
     (size: number) => {
@@ -52,7 +119,9 @@ export function ReacjinEditor() {
 
   return (
     <div className="absolute left-0 top-0 right-0 bottom-0 flex flex-col items-center">
-      <div className="text-2xl">reacjin editor</div>
+      <div className="text-2xl" title="rē-ˈak-shən">
+        reacjin
+      </div>
       <div className="flex flex-row gap-2 items-center p-2">
         <div>zoom</div>
         <input
@@ -70,7 +139,15 @@ export function ReacjinEditor() {
       </div>
       <div className="relative w-full h-full flex-1">
         <div className="absolute left-0 top-0 right-0 bottom-0 overflow-auto flex flex-col items-center justify-center">
-          <ImageCanvas width={imageSize[0]} height={imageSize[1]} zoom={zoom} />
+          <ImageCanvas
+            width={imageSize[0]}
+            height={imageSize[1]}
+            zoom={zoom}
+            layers={layers}
+          />
+        </div>
+        <div className="absolute right-0 top-0">
+          <LayerPane layers={layers} />
         </div>
       </div>
     </div>
--- src/app/reacjin/page.tsx
+++ src/app/reacjin/page.tsx
@@ -1,4 +1,11 @@
+import {Metadata} from 'next';
+
 import {ReacjinEditor} from '@/app/reacjin/ReacjinEditor';
+
+export const metadata: Metadata = {
+  title: 'reacjin',
+  description: 'reacji editor and tools',
+};
 
 export default function ReacjinPage() {
   return (
